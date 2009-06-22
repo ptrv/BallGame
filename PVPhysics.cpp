64438666e50adc84602b13c2cb7ccface0cfd442
lokijuhz <dubb3000@gmail.com>
2009-06-23 00:08:33 +0200

    - started implementing collision detection (does not work yet)

--- PVPhysics.cpp
+++ PVPhysics.cpp
@@ -23,8 +23,10 @@ void PVPhysics::simulate(std::map<std::string, PVNode*>& objectMap, std::vector<
 	for(unsigned int i = 0; i < numberOfFlyingBalls; ++i)
 	{
 		move(balls.at(i), time);
-		std::cout << time << std::endl;
+		//std::cout << time << std::endl;
 	}
+	
+	this->collisionDetection(objectMap, balls, numberOfFlyingBalls);
 }
 
 void PVPhysics::move(PVNode* ball, float time)
@@ -42,4 +44,100 @@ void PVPhysics::move(PVNode* ball, float time)
 		ball->setAcceleration(GRAVITY);
 	}
 					  
+}
+
+void PVPhysics::collisionDetection(std::map<std::string, PVNode*>& objectMap, std::vector<PVNode*>& balls, int numberOfFlyingBalls)
+{
+	std::map<std::string, PVNode*>::iterator itArena = objectMap.find("arena");
+	std::map<std::string, PVNode*>::iterator itWall = objectMap.find("wall");
+	std::map<std::string, PVNode*>::iterator itCannon = objectMap.find("kanone");
+	
+	for (unsigned int i = 0; i <= numberOfFlyingBalls; ++i) {
+		collisionWithArena(balls.at(i), itArena->second);
+	}
+}
+
+void PVPhysics::collisionWithArena(PVNode *arena, PVNode *ball)
+{
+	float arenaRadius = (arena->getSceneNode()->_getWorldAABB().getSize()).x / 2;
+	float arenaHeight = (arena->getSceneNode()->_getWorldAABB().getSize()).y;
+	float ballRadius = (ball->getSceneNode()->_getWorldAABB().getSize()).x / 2;
+	float toleranz = ballRadius + ballRadius;
+	Vector3 ballPosition = ball->getPosition();
+	
+	if (ballPosition.length() >= (arenaRadius - toleranz)) 
+	{
+		return;
+	}
+	
+	if (ballPosition.y >= arenaHeight) 
+	{
+		return;
+	}
+	
+	if (ballPosition.length() > (arenaRadius - toleranz)) 
+	{
+		handleCollision(arena, ball);
+		return;
+	}
+	
+	if ((ballPosition.y - ballRadius) < 0) 
+	{
+		handleCollisionWithGround(arena, ball);
+		return;
+	}
+}
+
+void PVPhysics::collisionWithWall(PVNode *wall, PVNode *ball)
+{
+}
+
+void PVPhysics::collisionWithCannon(PVNode *kanone, PVNode *ball)
+{
+}
+
+void PVPhysics::collisionWithBall(PVNode *ball1, PVNode *ball2)
+{
+}
+
+void PVPhysics::handleCollision(PVNode *fixedNode, PVNode *ball)
+{
+	Vector3 normale = fixedNode->getNormal();
+	Vector3 ballVelocity = ball->getVelocity();
+	
+	ballVelocity = ballVelocity.reflect(normale);
+	
+	Vector3 normalizedVelocity = ballVelocity.normalisedCopy();
+	float length = ball->getSceneNode()->_getWorldAABB().getSize().length();
+	ball->setPosition(ball->getPosition() + normalizedVelocity * length);
+
+	ball->setVelocity(ballVelocity * DAMPING);
+}
+
+void PVPhysics::handleCollisionWithGround(PVNode * fixedNode, PVNode *ball)
+{
+	Vector3 normale = Vector3(0.0, 1.0, 0.0);
+	Vector3 ballVelocity = ball->getVelocity();
+	
+	ballVelocity = ballVelocity.reflect(normale);
+	
+	Vector3 normalizedVelocity = ballVelocity.normalisedCopy();
+	float length = ball->getSceneNode()->_getWorldAABB().getSize().length();
+	ball->setPosition(ball->getPosition() + normalizedVelocity * length);
+	
+	ball->setVelocity(ballVelocity * DAMPING);
+}
+
+void PVPhysics::handleCollisionWithBall(PVNode *ball1, PVNode *ball2)
+{
+}
+
+Vector3 PVPhysics::invert(Vector3 vec)
+{
+	Vector3 result;
+	result.x = -vec.x;
+	result.y = -vec.y;
+	result.z = -vec.z;
+	
+	return result;
 }
--- PVPhysics.h
+++ PVPhysics.h
@@ -2,6 +2,9 @@
  Copyright (c) Peter Vasil, 2008
  ==========================================================*/
 
+#ifndef _PVPHYSICS_H_
+#define _PVPHYSICS_H_
+
 //#include <Carbon/Carbon.h>
 //#include "ExampleApplication.h"
 //#include <Carbon/Carbon.h>
@@ -24,6 +27,21 @@ class PVPhysics
 		void simulate(std::map<std::string, PVNode*>& objectMap, std::vector<PVNode*>& balls, int numberOfFlyingBalls, float time);
 		void move(PVNode* ball, float time);
 		
+		void collisionDetection(std::map<std::string, PVNode*>& objectMap, std::vector<PVNode*>& balls, int numberOfFlyingBalls);
+		
+		void collisionWithArena(PVNode *arena, PVNode *ball);
+		void collisionWithWall(PVNode *wall, PVNode *ball);
+		void collisionWithCannon(PVNode *kanone, PVNode *ball);
+		void collisionWithBall(PVNode *ball1, PVNode *ball2);
+		
+		void handleCollision(PVNode *fixedNode, PVNode *ball);
+		void handleCollisionWithGround(PVNode * fixedNode, PVNode *ball);
+		void handleCollisionWithBall(PVNode *ball1, PVNode *ball2);
+		
+		Vector3 invert(Vector3 vec);
+		
 	private:
 		
 	};
+
+#endif // _PVPHYSICS_H_
